099772c402356e3e93246b6ccc03b6b47394d182
VIMLAN GANESAN <gvimlan95@gmail.com>
2017-10-24 13:27:59 +0100

    code to split batches into single batch based on index function added

--- general/kFoldClassification.m
+++ general/kFoldClassification.m
@@ -1,8 +1,8 @@
 % Author: VIMLAN G
-% parameters(data,kFold), returns two matrices with dimension - 
-% training matrix[kFold,150-batchSize,132]
+% parameters(data,kFold)
+% returns two matrices with dimension - training matrix[kFold,150-batchSize,132] and
 % testing matrix [kFold,batchSize,132]
-function [training_batch,validation_batch] = kFoldClassification(data,kFold)
+function [training_batches,validation_batches] = kFoldClassification(data,kFold)
     batchSize = floor(size(data,1)/kFold);
     for c = 0:kFold-1
         tempData = data;
@@ -10,6 +10,6 @@ function [training_batch,validation_batch] = kFoldClassification(data,kFold)
         tempData((c*batchSize)+1:((c+1)*batchSize),:) = [];
         training(c+1,:,:) = tempData;
     end
-    training_batch = training;
-    validation_batch =  validation;
+    training_batches = training;
+    validation_batches =  validation;
 end
