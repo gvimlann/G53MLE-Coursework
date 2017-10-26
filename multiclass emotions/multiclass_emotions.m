dc1969f69a42be9fb6f11dea924a7380a4dd98e6
boeyjw <psyjwb@nottingham.ac.uk>
2017-10-26 10:58:39 +0100

    Added NN training params tuning constants. K-fold loop still has problems

--- multiclass emotions/multiclass_emotions.m
+++ multiclass emotions/multiclass_emotions.m
@@ -6,8 +6,8 @@ train_test_split_ratio = 0.2; % ratio for splitting train and test set
 
 % Create data
 load emotions_data.mat
-X = x';
-Y = label_encode(y)';
+X = x'; % features
+Y = label_encode(y)'; % labels
 
 % cross validation
 [train test] = crossvalind('HoldOut', length(X), train_test_split_ratio);
@@ -28,15 +28,39 @@ net_pf = 'softmax'; % NN performance function
 net_ipf = {}; % NN row cell array input processing function
 net_opf = {}; % NN row cell array output processing function
 net_ddf = {}; % NN data diversion function
+
 net = newff(net_p, net_t, net_si, net_tfi, net_btf, net_blf, net_pf);
 
-% NN params
+% NN general params
+net.trainParam.showWindow = true;
+net.trainParam.showCommandLine = false;
+net.trainParam.show = 25;
 net.trainParam.epochs = 1000;
+net.trainParam.time = inf;
+net.trainParam.goal = 0;
+net.trainParam.min_grad = 1e-07;
+net.trainParam.max_fail = 6;
+% NN training params
+switch net_btf
+    case {'trainlm'}
+        %net.efficiency.memoryReduction = 2; % Reduce memory usage of trainlm
+        net.trainParam.mu = 0.001;
+        net.trainParam.mu_dec = 0.1;
+        net.trainParam.mu_inc = 10;
+        net.trainParam.mu_max = 10000000000;
+    case {'traingd' 'traingda'}
+        net.trainParam.lr = 0.01;
+    case {'traingda'}
+        net.trainParam.lr_inc = 1.05;
+        net.trainParam.lr_dec = 0.7;
+        net.trainParam.max_perf_inc = 1.04;
+end
 
 % train NN
 % for i = 1:k_fold_cnt
 %     k_test_indices = (train_indices == i);
 %     k_train_indices = ~k_test_indices;
-%     net_output = train(net, X_train(:, k_train_indices), Y_train(:, k_train_indices));
+%     [net tr] = train(net, X_train(:, k_train_indices), Y_train(:, k_train_indices));
+%     plotperform(tr);
 % end
 
