c1c19cf15e237597e44dcd363c1c9afe330cae04
boeyjw <psyjwb@nottingham.ac.uk>
2017-10-27 18:42:57 +0100

    Alternative kfold&holdout that returns indices instead
    Overcomes @Vimlan high memory usage
    Single responsibility function for transparency

---
+++ general/holdout.m
@@ -0,0 +1,21 @@
+function [htrain,htest] = holdout(data,test_ratio)
+    if ~isscalar(data)
+        data = length(data);
+    end
+    if ~isa(test_ratio, 'numeric')
+        disp('Test data ratio must be between 0 and 1');
+        return;
+    end
+    
+    rd = round(data * test_ratio);
+    htrain = zeros([data 1]);
+    htest = zeros([data 1]);
+    test_perm_idx = randperm(data, rd)';
+    
+    for i = 1:rd
+        htest(test_perm_idx(i)) = 1;
+    end
+    htrain = ~htest;
+        
+end
+
