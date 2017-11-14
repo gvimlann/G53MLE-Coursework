cebe0ab657b4ce31ad92f90da1fe5a878e41dd54
boeyjw <psyjwb@nottingham.ac.uk>
2017-11-14 10:54:34 +0000

    Created k-fold script and pending tree evaluation
    For some reason, tree makes decision by 1's and 0's so its an error on
    the script side which will be fixed.

---
+++ Assignment 2/Decision Tree Learning/kfoldcross.m
@@ -0,0 +1,31 @@
+% @author Boey
+% Segments data into k folds by returning indices carrying value for each
+% fold
+% Vector segmentation is done using random permutations to ensure
+% randomness of indices
+function kindices = kfoldcross(data, k)
+    if ~isscalar(data)
+        data = size(data, 2);
+    end
+    if ~isscalar(k)
+        disp('K-value must be scalar');
+        return;
+    end
+    
+    kindices = zeros([data 1]);
+    perm_idx = randperm(data)';
+    idx = repmat((1:1:k)', round(data / k), 1);
+    
+    if length(idx) < data
+        for i = length(idx) + 1:1:data
+            idx(i) = randi(k);
+        end
+    elseif length(idx) > data
+        idx = idx(1:data);
+    end
+        
+    for i = 1:length(kindices)
+        kindices(perm_idx(i)) = idx(i);
+    end
+end
+
---
+++ Assignment 2/Decision Tree Learning/run_dtl.m
@@ -0,0 +1,24 @@
+% Data creation
+load('labels.mat');
+load('facialPoints.mat');
+p_size = size(points);
+X = reshape(points, [p_size(1)*p_size(2) p_size(3)])';
+Y = labels;
+
+% constants
+kfolds = 5;
+
+k_ind = kfoldcross(X, kfolds);
+
+sum_confusion = zeros([2 2]);
+for i = 1:kfolds
+    test_ind = (k_ind == i);
+    train_ind = ~test_ind;
+    trained_tree = decision_tree_learning(X(train_ind), Y(train_ind));
+    DrawDecisionTree(trained_tree, ['Tree_' num2str(i)]);
+    % test_output = evalTree(trained_tree, X(test_ind))
+%     [misclassified,cm,~,rates] = confusion(Y(test_ind), test_output);
+%     sum_confusion = sum_confusion + cm;
+end
+
+% [acc,rec,pre,f1] = confusion_rates(sum_confusion);
