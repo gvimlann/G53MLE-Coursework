629bd0b947078662350d55b1f544eba8254d7754
boeyjw <psyjwb@nottingham.ac.uk>
2017-11-20 12:50:58 +0000

    Added Name-Value pair argument for SVM function

--- Assignment_3/SVM.m
+++ Assignment_3/SVM.m
@@ -1,22 +1,32 @@
-function model = SVM(classification_type,X,Y)
-
+function model = SVM(X, Y, classification_type, varargin)
+    p = inputParser;
+    
+    defaultKernalScale = 1;
+    defaultPolynomialOrder = 3;
+    defaultEpsilon = iqr(Y) / 13.49;
+    
+    addParameter(p, 'KernelScale', defaultKernalScale, @isnumeric);
+    addParameter(p, 'PolynomialOrder', defaultPolynomialOrder, @isnumeric);
+    addParameter(p, 'Epsilon', defaultEpsilon, @isnumeric);
+    parse(p, varargin{:});
+    
     if strcmpi(classification_type, 'linear_classification')
-        model = fitcsvm(X,Y, 'KernelFunction','linear', 'BoxConstraint',1);
+        model = fitcsvm(X, Y, 'KernelFunction', 'linear', 'BoxConstraint', 1);
     
     elseif strcmpi(classification_type, 'linear_regression')
-        model = fitrsvm(X,Y, 'KernelFunction','linear', 'BoxConstraint',1, 'Epsilon', 1);
+        model = fitrsvm(X, Y, 'KernelFunction', 'linear', 'BoxConstraint', 1, 'Epsilon', p.Results.Epsilon);
         
     elseif strcmpi(classification_type, 'polynomial_classification')
-        model = fitcsvm(X,Y, 'KernelFunction','polynomial', 'PolynomialOrder','q', 'BoxConstraint',1);
+        model = fitcsvm(X, Y, 'KernelFunction', 'polynomial', 'PolynomialOrder', p.Results.PolynomialOrder, 'BoxConstraint', 1);
         
     elseif strcmpi(classification_type, 'polynomial_regression')
-        model = fitrsvm(X,Y, 'KernelFunction','polynomial', 'PolynomialOrder','q', 'BoxConstraint',1, 'Epsilon', 1);   
+        model = fitrsvm(X, Y, 'KernelFunction', 'polynomial', 'PolynomialOrder', p.Results.PolynomialOrder, 'BoxConstraint', 1, 'Epsilon', p.Results.Epsilon);   
     
     elseif strcmpi(classification_type, 'rbf_classification')
-        model = fitcsvm(X,Y, 'KernelFunction', 'rbf', 'KernelScale','sigma', 'BoxConstraint',1);
+        model = fitcsvm(X, Y, 'KernelFunction', 'rbf', 'KernelScale', p.Results.KernelScale, 'BoxConstraint', 1);
         
     elseif strcmpi(classification_type, 'rbf_regression')
-        model = fitrsvm(X,Y, 'KernelFunction','rbf', 'KernelScale','sigma', 'BoxConstraint',1, 'Epsilon', 1);
+        model = fitrsvm(X, Y, 'KernelFunction', 'rbf', 'KernelScale', p.Results.KernelScale, 'BoxConstraint', 1, 'Epsilon', p.Results.Epsilon);
         
     end
 end
