60fbcba1c3a8359d59dc987d7e6c556fb1e4391c
Abdul Hazim <abdhazim69@gmail.com>
2017-11-20 11:48:52 +0000

    adjusted SVM.m. q is meant to be order of q (defined by us). sigma also needs to be defined by us.

--- Assignment_3/SVM.m
+++ Assignment_3/SVM.m
@@ -7,16 +7,16 @@ function model = SVM(classification_type,X,Y)
         model = fitrsvm(X,Y, 'KernelFunction','linear', 'BoxConstraint',1, 'Epsilon', 1);
         
     elseif strcmpi(classification_type, 'polynomial_classification')
-        model = fitcsvm(X,Y, 'KernelFunction','polynomial', 'PolynomialOrder','q', 'BoxConstraint',1);
+        model = fitcsvm(X,Y, 'KernelFunction','polynomial', 'PolynomialOrder', q, 'BoxConstraint',1);
         
     elseif strcmpi(classification_type, 'polynomial_regression')
-        model = fitrsvm(X,Y, 'KernelFunction','polynomial', 'PolynomialOrder','q', 'BoxConstraint',1, 'Epsilon', 1);   
+        model = fitrsvm(X,Y, 'KernelFunction','polynomial', 'PolynomialOrder', q, 'BoxConstraint',1, 'Epsilon', 1);
     
     elseif strcmpi(classification_type, 'rbf_classification')
-        model = fitcsvm(X,Y, 'KernelFunction', 'rbf', 'KernelScale','sigma', 'BoxConstraint',1);
+        model = fitcsvm(X,Y, 'KernelFunction', 'rbf', 'KernelScale', sigma, 'BoxConstraint',1);
         
     elseif strcmpi(classification_type, 'rbf_regression')
-        model = fitrsvm(X,Y, 'KernelFunction','rbf', 'KernelScale','sigma', 'BoxConstraint',1, 'Epsilon', 1);
+        model = fitrsvm(X,Y, 'KernelFunction','rbf', 'KernelScale', sigma, 'BoxConstraint',1, 'Epsilon', 1);
         
     end
 end
