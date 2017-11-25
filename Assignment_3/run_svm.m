ed858c06cc84cd2c9d68fe84cea9c66105d5843b
Abdul Hazim <abdhazim69@gmail.com>
2017-11-25 12:39:41 +0000

    Found a way to run SVMs efficiently with inputParsers

--- Assignment_3/run_svm.m
+++ Assignment_3/run_svm.m
@@ -1,12 +1,11 @@
-% This is the MAIN script that generates one SVM via inner-crossfold validation.
+% This is the MAIN script that generates THREE SVMs via inner-crossfold validation.
+% 
 % Random search is performed for hyperparameter optimisation
 
 % constants
 IS_REGRESSION = 0;
 OUTER_kFOLD = 10;
 INNER_kFOLD = 10;
-% Kernel to be used: 'linear' OR 'polynomial' OR 'rbf'
-KERNEL_TYPE = 'linear';
 
 % load and transform data
 load('facialPoints.mat');
@@ -20,15 +19,59 @@ end
 spoints = size(points);
 X = reshape(points, [spoints(1)*spoints(2) spoints(3)])';
 
-% variables
-% SVM storage during hyperparameter optimisation
-svm_store = {};
+% Default values for SVM
+defaultKernalScale = 1;
+defaultPolynomialOrder = 3;
+defaultEpsilon = iqr(Y) / 13.49;
+
+% Additional variables for polynomial and rbg
+linear_input = inputParser;
+poly_input = inputParser;
+rbg_input = inputParser;
 if IS_REGRESSION
     % Vars for regression
-    
+    linear_Kernel = 'linear_regression';
+    poly_Kernel = 'polynomial_regression';
+    rbg_Kernel = 'rbg_regression';
+
+    % Additional vars for rbg
+    addParameter(rbg_input, 'KernelScale', defaultKernalScale, @isnumeric);
+    addParameter(rbg_input, 'Epsilon', defaultEpsilon, @isnumeric);
+
+    % Additional vars for polynomial
+    addParameter(poly_input, 'PolynomialOrder', defaultPolynomialOrder, @isnumeric);
+    addParameter(poly_input, 'Epsilon', defaultEpsilon, @isnumeric);
+
+    % Additional vars for linear
+    addParameter(linear_input, 'Epsilon', defaultEpsilon, @isnumeric);
 else
     % Vars for binary
-    
+    linear_Kernel = 'linear_classification';
+    poly_Kernel = 'polynomial_classification';
+    rbg_Kernel = 'rbf_classification';
+
+    % Additional vars for rbg
+    addParameter(rbg_input, 'KernelScale', defaultKernalScale, @isnumeric);
+
+    % Additional vars for polynomial
+    addParameter(poly_input, 'PolynomialOrder', defaultPolynomialOrder, @isnumeric);
 end
 
+parse(linear_input);
+parse(poly_input);
+parse(rbg_input);
+
 % cross-validate
+for i=1:OUTER_kFOLD
+    % Set up model in this loop
+    svm_linear = SVM(X, Y, linear_Kernel, linear_input.Results);
+    svm_poly = SVM(X, Y, poly_Kernel, poly_input.Results);
+    svm_rbg = SVM(X, Y, rbg_Kernel, rbg_input.Results);
+
+    for j=1:INNER_kFOLD
+        % Test out model in this loop
+        
+    end
+
+    % Random Search algorithm
+end
