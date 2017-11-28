76683a217f4c41a1e1ba84563df41d875af0cb04
Abdul Hazim <abdhazim69@gmail.com>
2017-11-28 19:05:52 +0000

    It now keeps track of average errors for each parameter change.

--- Assignment_3/run_svm.m
+++ Assignment_3/run_svm.m
@@ -5,7 +5,7 @@
 
 % constants
 IS_REGRESSION = 0;
-OUTER_kFOLD = 3;
+NUM_OF_TRIALS = 5;
 INNER_kFOLD = 5;
 
 % Min Constants
@@ -15,10 +15,10 @@ MIN_KS = 1;
 
 % Max Constants
 MAX_EPSILON = 2;
-MAX_POLY = 32;
-MAX_KS = 32;
+MAX_POLY = 12;
+MAX_KS = 12;
 
-SEED = 1337;
+SEED = 432;
 % Initialise random number generator with seed
 rng(SEED);
 
@@ -69,15 +69,54 @@ missclassification_poly = zeros([INNER_kFOLD 1]);
 missclassification_rbg = zeros([INNER_kFOLD 1]);
 
 % To keep track of parameters
-history_epsilon = zeros([OUTER_kFOLD 1]);
-history_polyOrder = zeros([OUTER_kFOLD 1]);
-history_kernelScale = zeros([OUTER_kFOLD 1]);
+history_epsilon = zeros([NUM_OF_TRIALS 1]);
+history_polyOrder = zeros([NUM_OF_TRIALS 1]);
+history_kernelScale = zeros([NUM_OF_TRIALS 1]);
+
+% To keep track of average Errors
+average_err_linear = zeros([NUM_OF_TRIALS 1]);
+average_err_poly = zeros([NUM_OF_TRIALS 1]);
+average_err_rbg = zeros([NUM_OF_TRIALS 1]);
 
 % OUTER FOLD = Number of Trials
-for i=1:OUTER_kFOLD
+for i=1:NUM_OF_TRIALS
+
+    % Random Search algorithm for SVM configurations    
+    % Randomly generate parameters for each SVM {linear, poly, rbf}
+    if IS_REGRESSION
+        % Randomise/Tweak Epsilon
+        epsilon = MIN_EPSILON + rand()*(MAX_EPSILON-MIN_EPSILON);
+        history_epsilon(i) = epsilon;
+        % Set up new linear SVM arguments       
+        linear_val = {'Epsilon', epsilon};
+
+        % Randomise/Tweak polynomial order
+        polynomial_order = ceil(MIN_POLY + rand()*(MAX_POLY-MIN_POLY));
+        history_polyOrder(i) = polynomial_order;
+        % set up new polynomial SVM configuration
+        poly_val = {'Epsilon', epsilon, 'PolynomialOrder', polynomial_order};
+
+        % Randomise/Tweak rbg Kernel Scale
+        kernel_scale = ceil(MIN_KS + rand()*(MAX_KS-MIN_KS));
+        history_kernelScale(i) = kernel_scale;
+        % set up new rbg SVM configuration
+        rbg_val = {'Epsilon', epsilon, 'KernelScale', kernel_scale};
+    else        
+        % Randomise/Tweak polynomial order
+        polynomial_order = ceil(MIN_POLY + rand()*(MAX_POLY-MIN_POLY));
+        history_polyOrder(i) = polynomial_order;
+        % set up new polynomia SVM configuration
+        poly_val = {'PolynomialOrder', polynomial_order};
+        
+        % Randomise/Tweak rbg Kernel Scale
+        kernel_scale = ceil(MIN_KS + rand()*(MAX_KS-MIN_KS));
+        history_kernelScale(i) = kernel_scale;
+        % set up new rbg SVM configuration
+        rbg_val = {'KernelScale', kernel_scale};
+    end
 
     for j=1:INNER_kFOLD
-        % Test out model in this loop
+        % Train and Test out models in this loop
         % data segmentation
         k_test_indices = (train_indices == j);
         k_train_indices = ~k_test_indices;
@@ -117,32 +156,33 @@ for i=1:OUTER_kFOLD
         end
     end
 
+    % Determine the performance of this randomly generated model
     if IS_REGRESSION
         % Calculate the average mean squared error
-        average_MSE_linear = mean(mean_square_err_linear);
-        average_MSE_poly = mean(mean_square_err_poly);
-        average_MSE_rbg = mean(mean_square_err_rbg);
+        average_err_linear(i) = mean(mean_square_err_linear);
+        average_err_poly(i) = mean(mean_square_err_poly);
+        average_err_rbg(i) = mean(mean_square_err_rbg);
 
         % to keep track of the best accuracy/score
         if i == 1
-            score_linear_best = average_MSE_linear;
-            score_poly_best = average_MSE_poly;
-            score_rbg_best = average_MSE_rbg;
+            score_linear_best = average_err_linear(i);
+            score_poly_best = average_err_poly(i);
+            score_rbg_best = average_err_rbg(i);
             svm_linear_best = svm_linear;
             svm_poly_best = svm_poly;
             svm_rbg_best = svm_rbg;
         else 
             % Check if it is a better model
-            if average_MSE_linear < score_linear_best
-                score_linear_best = average_MSE_linear;
+            if average_err_linear(i) < score_linear_best
+                score_linear_best = average_err_linear(i);
                 svm_linear_best = svm_linear;
             end
-            if average_MSE_poly < score_poly_best
-                score_poly_best = average_MSE_poly;
+            if average_err_poly(i) < score_poly_best
+                score_poly_best = average_err_poly(i);
                 svm_poly_best = svm_poly;
             end
-            if average_MSE_rbg < score_rbg_best
-                score_rbg_best = average_MSE_rbg;
+            if average_err_rbg(i) < score_rbg_best
+                score_rbg_best = average_err_rbg(i);
                 svm_rbg_best = svm_rbg;
             end
         end
@@ -153,67 +193,34 @@ for i=1:OUTER_kFOLD
         [acc_rbg,rec_rbg,pre_rbg,f1_rbg] = confusion_rates(sum_confusion_rbg);
 
         % average misclassification error
-        average_MCE_linear = mean(missclassification_linear);
-        average_MCE_poly = mean(missclassification_poly);
-        average_MCE_rbg = mean(missclassification_rbg);
+        average_err_linear(i) = mean(missclassification_linear);
+        average_err_poly(i) = mean(missclassification_poly);
+        average_err_rbg(i) = mean(missclassification_rbg);
 
         % to keep track of the best accuracy/score
         if i == 1
-            score_linear_best = average_MCE_linear;
-            score_poly_best = average_MCE_poly;
-            score_rbg_best = average_MCE_rbg;
+            score_linear_best = average_err_linear(i);
+            score_poly_best = average_err_poly(i);
+            score_rbg_best = average_err_rbg(i);
             svm_linear_best = svm_linear;
             svm_poly_best = svm_poly;
             svm_rbg_best = svm_rbg;
         else 
             % Check if it is a better model, save it if it is
-            if average_MCE_linear < score_linear_best
-                score_linear_best = average_MCE_linear;
+            if average_err_linear(i) < score_linear_best
+                score_linear_best = average_err_linear(i);
                 svm_linear_best = svm_linear;
             end
-            if average_MCE_poly < score_poly_best
-                score_poly_best = average_MCE_poly;
+            if average_err_poly(i) < score_poly_best
+                score_poly_best = average_err_poly(i);
                 svm_poly_best = svm_poly;
             end
-            if average_MCE_rbg < score_rbg_best
-                score_rbg_best = average_MCE_rbg;
+            if average_err_rbg(i) < score_rbg_best
+                score_rbg_best = average_err_rbg(i);
                 svm_rbg_best = svm_rbg;
             end
         end
     end 
-
-    % Random Search algorithm for the next SVM configurations    
-    if IS_REGRESSION
-        % Randomise/Tweak Epsilon
-        epsilon = MIN_EPSILON + rand()*(MAX_EPSILON-MIN_EPSILON);
-        history_epsilon(i) = epsilon;
-        % Set up new linear SVM arguments       
-        linear_val = {'Epsilon', epsilon};
-
-        % Randomise/Tweak polynomial order
-        polynomial_order = ceil(MIN_POLY + rand()*(MAX_POLY-MIN_POLY));
-        history_polyOrder(i) = polynomial_order;
-        % set up new polynomial SVM configuration
-        poly_val = {'Epsilon', epsilon, 'PolynomialOrder', polynomial_order};
-
-        % Randomise/Tweak rbg Kernel Scale
-        kernel_scale = ceil(MIN_KS + rand()*(MAX_KS-MIN_KS));
-        history_kernelScale(i) = kernel_scale;
-        % set up new rbg SVM configuration
-        rbg_val = {'Epsilon', epsilon, 'KernelScale', kernel_scale};
-    else        
-        % Randomise/Tweak polynomial order
-        polynomial_order = ceil(MIN_POLY + rand()*(MAX_POLY-MIN_POLY));
-        history_polyOrder(i) = polynomial_order;
-        % set up new polynomia SVM configuration
-        poly_val = {'PolynomialOrder', polynomial_order};
-        
-        % Randomise/Tweak rbg Kernel Scale
-        kernel_scale = ceil(MIN_KS + rand()*(MAX_KS-MIN_KS));
-        history_kernelScale(i) = kernel_scale;
-        % set up new rbg SVM configuration
-        rbg_val = {'KernelScale', kernel_scale};
-    end
 end
 
 % Plot parameters and accuracy
